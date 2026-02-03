69c0c01d11e025fe4a80d961f62e333cf7f64be1
montrea13x <montrea13x@gmail.com>
2026-02-03 15:54:32 -0500

    quelque modif

---
+++ unittests/PrivateKeyEncryptorTests.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace unittests;
+
+public class PrivateKeyEncryptorTests
+{
+    [Fact]
+    public void TestParseKeySize_Default()
+    {
+        
+    }
+}
